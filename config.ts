d73fc1101d510bc2638ea7f3e42e4c2ca12af2b0
jordan-mann <jr.mannsjn@.com>
2023-04-15 16:39:32 -0500

    configurating things

--- config.ts
+++ config.ts
@@ -1,13 +1,3 @@
 import 'dotenv/config';
 
-<<<<<<< HEAD
-const SESSION_SECRET = process.env.SESSION_SECRET || '';
-
-// module.exports = {
-//   SESSION_SECRET
-// }
-
-export default SESSION_SECRET;
-=======
 export const SESSION_SECRET = process.env.SESSION_SECRET || '';
->>>>>>> cbfcdee8a674ef679440877bca677bcce28950f9
