ff21c3d9759f9cfeb96120d0bcc6ca6d3f1fc363
magerman714 <magerman714@gmail.com>
2023-04-19 18:59:59 -0500

    Merge conflicts correction

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 import App from './components/App';
 import Home from './components/Home';
 import BulletinBoard from './components/BulletinBoard/BulletinBoard';
@@ -9,16 +10,60 @@ import CreateReport from './components/Reports/CreateReport';
 import RouteM from './components/BikeRoutes/RouteM';
 import Stopwatch from './components/Stopwatch';
 
+export interface CurrentWeather {
+  temperature: number;
+  windspeed: number;
+  winddirection: number;
+  weatherdescription: string;
+  is_day: number | undefined;
+  time: Date;
+}
+
+export interface MeasurementUnits {
+  temperature: string;
+  speed: string;
+  precipitation: string;
+  visibility: string;
+  depth: string;
+}
+
+export interface Hourly {
+  //May not need the "?"'s
+  time?: Date;
+  temperature?: number;
+  humidity?: number;
+  apparentTemperature?: number;
+  cloudcover?: number;
+  windspeed?: number;
+  precipitation?: number;
+  snowfall?: number;
+  precipitationProbability?: number;
+  rain?: number;
+  showers?: number;
+  weatherDescription?: string;
+  snowDepth?: number;
+  visibility?: number;
+  isDay?: Boolean;
+}
+
+// export interface ForecastProps extends RootProps {
+//   currentWeather?: CurrentWeather;
+//   hourlyForecasts: Hourly[];
+//   setCurrentWeather?: (unit: CurrentWeather) => void;
+//   setHourlyForecasts?: (unit: Hourly[]) => void;
+// }
 
 export interface RootProps {
   windSpeedMeasurementUnit: string;
   temperatureMeasurementUnit: string;
   precipitationMeasurementUnit: string;
-  setWindSpeedMeasurementUnit: (unit: string) => void;
-  setTemperatureMeasurementUnit: (unit: string) => void;
-  setPrecipitationMeasurementUnit: (unit: string) => void;
+  hourlyForecasts: Hourly[];
+  setHourlyForecasts?: (unit: Hourly[]) => void;
+  setWindSpeedMeasurementUnit?: (unit: string) => void;
+  setTemperatureMeasurementUnit?: (unit: string) => void;
+  setPrecipitationMeasurementUnit?: (unit: string) => void;
+  getForecasts?: () => void;
 }
-
 export interface StopwatchTime {
   hours: number;
   minutes: number;
@@ -32,6 +77,74 @@ const Root = () => {
     useState<string>('fahrenheit'); //should be either 'fahrenheit' or 'celsius'
   const [precipitationMeasurementUnit, setPrecipitationMeasurementUnit] =
     useState<string>('inch'); //should be either 'mm' or 'inch'
+  const [currentWeather, setCurrentWeather] = useState<CurrentWeather>({
+    temperature: 0,
+    windspeed: 0,
+    winddirection: 0,
+    weatherdescription: '',
+    is_day: undefined,
+    time: new Date(),
+  }); //note: currentWeather is only going to be used on the home screen - everything else will just use the hourly breakdown
+
+  const [hourlyForecasts, setHourlyForecasts] = useState<Hourly[]>([]);
+
+  // const weatherCodes = {
+  //   //these are what each of the weather codes mean; no codes should be missing even though there are gaps between numbers
+  //   0: 'Clear Sky',
+  //   1: 'Mainly Clear',
+  //   2: 'Partly Cloudy',
+  //   3: 'Overcast',
+  //   45: 'Fog',
+  //   48: 'Depositing Rime Fog',
+  //   51: 'Light Drizzle',
+  //   53: 'Moderate Drizzle',
+  //   55: 'Dense Drizzle',
+  //   56: 'Light Freezing Drizzle',
+  //   57: 'Dense Freezing Drizzle',
+  //   61: 'Light Rain',
+  //   63: 'Moderate Rain',
+  //   65: 'Heavy Rain',
+  //   71: 'Light Snow',
+  //   73: 'Moderate Snow',
+  //   75: 'Heavy Snow',
+  //   77: 'Snow Grains',
+  //   80: 'Light Showers',
+  //   81: 'Moderate Showers',
+  //   82: 'Violent Showers',
+  //   85: 'Light Snow Showers',
+  //   86: 'Heavy Snow Showers',
+  //   95: 'Thunderstorm',
+  //   96: 'Thunderstorm With Light Hail',
+  //   99: 'Thunderstorm with Heavy Hail',
+  // };
+
+  const latitude = 30.0; //will need to be able to update lat/long according to inputted location
+  const longitude = -90.17;
+  const numDaysToForecast = 1; //this is for if we implement a weekly weather report
+  const getForecasts = () => {
+    //axios request has been tested and is working; states are properly being set with objects containing the required values
+    axios
+      .get('/weather/forecast', {
+        params: {
+          precipitationUnit: precipitationMeasurementUnit,
+          windSpeedUnit: windSpeedMeasurementUnit,
+          temperatureUnit: temperatureMeasurementUnit,
+          latitude: latitude,
+          longitude: longitude,
+          numDaysToForecast: numDaysToForecast,
+        },
+      })
+      .then(({ data }) => {
+        setCurrentWeather(data.currentWeather);
+        setHourlyForecasts(data.hourly);
+      })
+      .catch((err) =>
+        console.error(
+          'an error occured with clientside GET request for forecasts: ',
+          err
+        )
+      );
+  };
 
   return (
     <div>
@@ -48,17 +161,19 @@ const Root = () => {
                   windSpeedMeasurementUnit={windSpeedMeasurementUnit}
                   temperatureMeasurementUnit={temperatureMeasurementUnit}
                   precipitationMeasurementUnit={precipitationMeasurementUnit}
+                  hourlyForecasts={hourlyForecasts}
                   setWindSpeedMeasurementUnit={setWindSpeedMeasurementUnit}
                   setTemperatureMeasurementUnit={setTemperatureMeasurementUnit}
                   setPrecipitationMeasurementUnit={
                     setPrecipitationMeasurementUnit
                   }
+                  getForecasts={getForecasts}
                 />
               }
             />
             <Route path='profile' element={<Profile />} />
             <Route path='createReport' element={<CreateReport />} />
-            <Route path='stopwatch' element={<Stopwatch/>} />
+            <Route path='stopwatch' element={<Stopwatch />} />
           </Route>
         </Routes>
         <Stopwatch />
--- client/src/components/Weather/Weather.tsx
+++ client/src/components/Weather/Weather.tsx
@@ -1,25 +1,64 @@
 import React from 'react';
 import ForecastRow from './ForecastRow';
 import { RootProps } from '../../Root';
+import { Hourly } from '../../Root';
 
 const Weather = ({
   windSpeedMeasurementUnit,
   temperatureMeasurementUnit,
   precipitationMeasurementUnit,
+  hourlyForecasts,
   setWindSpeedMeasurementUnit,
   setTemperatureMeasurementUnit,
   setPrecipitationMeasurementUnit,
+  getForecasts,
 }: RootProps) => {
+  //need to break up hourly forecasts into chunks of 4 forecasts each for the ForecastRow to map through
+
+  let forecastRowArrays: Hourly[][] = [];
+  let forecastArray: Hourly[] = [];
+
+  hourlyForecasts.forEach((ele: Hourly, i: number) => {
+    if ((i + 1) % 4 === 0) {
+      forecastArray.push(ele);
+      forecastRowArrays.push(forecastArray);
+      forecastArray = new Array();
+    } else {
+      forecastArray.push(ele);
+    }
+  });
+  forecastRowArrays.push(forecastArray);
+
+  // let counter = 0;
+  // let hourlyIndex = 0;
+  // for(let i = 0; i < 6; i++){
+  //   let start = hourlyIndex;
+  //   let forecastRow = []
+  //   for(let i = start; i < start + 4; i++){
+  //     hourlyIndex = i;
+  //     forecastRow.push(hourlyForecasts[i]);
+  //   }
+  // }
+
   return (
     <div>
+      {forecastRowArrays.forEach((weatherRow) => {
+        <ForecastRow
+          key={weatherRow[0].time}
+          windSpeedMeasurementUnit={windSpeedMeasurementUnit}
+          temperatureMeasurementUnit={temperatureMeasurementUnit}
+          precipitationMeasurementUnit={precipitationMeasurementUnit}
+          hourlyForecasts={hourlyForecasts}
+          getForecasts={getForecasts}
+        />;
+      })}
       <ForecastRow
         windSpeedMeasurementUnit={windSpeedMeasurementUnit}
         temperatureMeasurementUnit={temperatureMeasurementUnit}
         precipitationMeasurementUnit={precipitationMeasurementUnit}
-        setWindSpeedMeasurementUnit={setWindSpeedMeasurementUnit}
-        setTemperatureMeasurementUnit={setTemperatureMeasurementUnit}
-        setPrecipitationMeasurementUnit={setPrecipitationMeasurementUnit}
-      ></ForecastRow>
+        hourlyForecasts={hourlyForecasts}
+        getForecasts={getForecasts}
+      />
     </div>
   );
 };
