fee9b1af968af3f0f217475c00b9c3c52c084f6a
opsparkbtc <brendantcarmichael@gmail.com>
2023-04-19 00:46:08 -0500

    BulletinBoard and Bulletin Components Built

--- client/src/components/BulletinBoard/BulletinBoard.tsx
+++ client/src/components/BulletinBoard/BulletinBoard.tsx
@@ -5,10 +5,8 @@ import axios from 'axios';
 import Bulletin from './Bulletin'
 
 
-//Request to retrieve all Bulletin Board Posts from the database
 
 const BulletinBoard = () => {
-
   //const context = useContext(UserContext);
   const [bulletins, setBulletins] = useState([]);
 
@@ -22,14 +20,27 @@ const BulletinBoard = () => {
     });
   };
 
-  useEffect(() => {
+  const handleBulletinSelection = (e) => {
+    axios.get('/api/bulletins')
+    .then((bulletinData) => {
+      setBulletins(bulletinData.data.filter((selected) => e.bulletinID === selected.bulletinID));
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  };
+
+
+
+    useEffect(() => {
     getAllBulletins();
   })
 
   return (
     <div>
       <div>Bulletins</div>
-      {bulletins.map((bulletin, i) => (<Bulletin bulletinData={bulletin} class='bulletin'/>))}
+      {bulletins.map((bulletin, i) => (<Bulletin bulletinData={bulletin}
+      handleBulletinSelection={ handleBulletinSelection } class='bulletin'/>))}
     </div>
   );
 };
