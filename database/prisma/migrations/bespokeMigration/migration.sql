50b7a3d0eb5330f39144d433cf9a68d77541372f
opsparkbtc <brendantcarmichael@gmail.com>
2023-04-20 03:13:37 -0500

    auth.js callback posts to DB

--- database/prisma/migrations/bespokeMigration/migration.sql
+++ database/prisma/migrations/bespokeMigration/migration.sql
@@ -16,13 +16,21 @@ CREATE TABLE "Report" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "published" BOOLEAN NOT NULL DEFAULT false,
-    "authorId" TEXT NOT NULL,
 
     CONSTRAINT "Report_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Bulletin" (
+    "id" TEXT NOT NULL,
+    "topic" TEXT NOT NULL,
+    "creator" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Bulletin_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- AddForeignKey
-ALTER TABLE "Report" ADD CONSTRAINT "Report_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
