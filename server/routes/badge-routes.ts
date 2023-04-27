6a0451252dc0e97fed57e367bac3d5cd724f569d
magerman714 <magerman714@gmail.com>
2023-04-26 20:37:37 -0500

    added array of seed objects for Badges in server-assets.ts, then imported it to badge-routes.ts to be used by the seeder function

--- server/routes/badge-routes.ts
+++ server/routes/badge-routes.ts
@@ -1,14 +1,25 @@
 import { Router, Request, Response } from 'express';
 import { PrismaClient, User, Badge, BadgesOnUsers } from '@prisma/client';
-
+import { badgesSeed } from '../server-assets';
 const badgeRouter = Router();
 const prisma = new PrismaClient();
 
 badgeRouter.patch('/counter', async (req: Request, res: Response) => {
   try {
     const { userId, key, change } = req.body;
+    let user: User | null = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+    let currentValue = 0;
+    if (!user) {
+      console.error('an error occurred trying to find user with id ', userId);
+    } else {
+      currentValue = user[key];
+    }
     let dataObj = {};
-    dataObj[key] = change; //this should actually be 'current value' + change; need add a method to find the current value of this key!
+    dataObj[key] = currentValue + change; //this should actually be 'current value' + change; need add a method to find the current value of this key!
     const updateCounter = await prisma.user.update({
       where: {
         id: userId,
@@ -24,5 +35,22 @@ badgeRouter.patch('/counter', async (req: Request, res: Response) => {
     res.sendStatus(500);
   }
 });
+
+//seeder function
+
+async function seedBadges() {
+  for (let i = 0; i < badgesSeed.length; i++) {
+    const badge = await prisma.badge.create({
+      data: {
+        name: badgesSeed[i].name,
+        badgeIcon: badgesSeed[i].badgeIcon,
+      },
+    });
+  }
+}
+
+seedBadges()
+  .catch((err) => console.error('an error occurred when seeding Badges: ', err))
+  .finally(() => prisma.$disconnect()); //unsure if this part is necessary?
 
 export { badgeRouter };
--- server/server-assets.ts
+++ server/server-assets.ts
@@ -27,3 +27,130 @@ export const weatherCodes = {
   96: 'Thunderstorm With Light Hail',
   99: 'Thunderstorm with Heavy Hail',
 };
+
+export const badgesSeed = [
+  {
+    name: 'Certified',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Bespoke Devotee',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Monthly Trekker',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Dedicated Ride',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Early Bird',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Night Rider',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Tourist',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'World Traveler',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Iron Lungs',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Extremophile',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Road Warrior',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Lean Machine',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Community Enthusiast',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Wheelie Popular',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Storm Chaser',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Fairweather Friend',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Safety Sentinel',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Speedster',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Centurion',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Social Butterfly',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Explorer',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Tour Guide',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Riding Streak',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Disciplined',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Karen',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Lean Legend',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Community Legend',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Traveling Legend',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Likable Legend',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Legendary Warden',
+    badgeIcon: 'url',
+  },
+  {
+    name: 'Legendary Explorer',
+    badgeIcon: 'url',
+  },
+];
