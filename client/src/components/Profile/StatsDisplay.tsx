2dacb7cdf4ca82de56708afb8a29ed192e71b726
jordan-mann <jr.mannsjn@.com>
2023-05-25 17:45:05 -0500

    styling on stats display grid

--- client/src/components/Profile/StatsDisplay.tsx
+++ client/src/components/Profile/StatsDisplay.tsx
@@ -1,42 +1,44 @@
 import * as React from 'react';
 import axios from 'axios';
+import { UserContext } from '../../Root';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid, GridColDef, GridPagination } from '@mui/x-data-grid';
 import dayjs from 'dayjs';
 import { duration } from '@mui/material';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { makeStyles } from '@mui/styles';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import Fab from '@mui/material/Fab';
 
 const StatsDisplay = ({ stats, handleGridClose, handleClickOpen }) => {
   const handleBackClick = () => {
     handleGridClose();
   };
 
+  // User Context //
+  const { isDark } = React.useContext(UserContext);
 
-const statsWithIds = stats.reverse().map((row, index) => {
+  const statsWithIds = stats.reverse().map((row, index) => {
+    const hours = Math.floor(row.duration / 60);
+    const minutes = row.duration % 60;
 
-  const hours = Math.floor(row.duration / 60);
-  const minutes = row.duration % 60;
+    if (hours === 0) {
+      row.duration = `${minutes}m`;
+    } else if (minutes === 0) {
+      row.duration = `${hours}h`;
+    } else {
+      row.duration = `${hours}h ${minutes}m`;
+    }
 
-  if (hours === 0) {
-    row.duration = `${minutes}m`
-  } else if (minutes === 0) {
-    row.duration =  `${hours}h`
-  } else {
-    row.duration = `${hours}h ${minutes}m`
-  }
-
- return {
-  ...row,
-  id: row.id,
-    duration: row.duration,
-}
-
-});
-
-
+    return {
+      ...row,
+      id: row.id,
+      duration: row.duration,
+    };
+  });
 
   const [rows, setRows] = React.useState(statsWithIds);
 
@@ -70,50 +72,118 @@ const statsWithIds = stats.reverse().map((row, index) => {
       field: 'delete',
       headerName: 'Delete',
       renderCell: (params) => (
-        <IconButton aria-label="delete">
-           <DeleteIcon  onClick={() => {
-            // Filter out the row with the clicked button's id and update state
-            setRows(rows.filter((row) => row.id !== params.id));
-            deleteStat(params.id);
-          }}  />
+        <IconButton aria-label='delete'>
+          <DeleteIcon
+            sx={{ color: isDark ? '#eae9e9' : '#555555' }}
+            onClick={() => {
+              // Filter out the row with the clicked button's id and update state
+              setRows(rows.filter((row) => row.id !== params.id));
+              deleteStat(params.id);
+            }}
+          />
         </IconButton>
       ),
       width: 100,
     },
   ];
 
-
-
   const deleteStat = (id) => {
-    // stats.map((stat) => {
-      // console.log('stat', stat)
-      axios.delete(`/profile/deleteStat/${id}`)
-        .then(() => {console.log('successfully deleted')})
-        .catch((err) => {
-          console.log(err);
-        })
-    // })
+    axios
+      .delete(`/profile/deleteStat/${id}`)
+      .then(() => {
+        console.log('successfully deleted');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
+  const CustomPagination = (props) => {
+    const paginationColor = isDark ? 'white' : 'black';
 
-  }
+    const theme = createTheme({
+      components: {
+        MuiPaginationItem: {
+          styleOverrides: {
+            root: {
+              color: paginationColor,
+              '&:hover': {
+                backgroundColor: paginationColor,
+                color: 'white',
+              },
+              '&.Mui-selected': {
+                backgroundColor: paginationColor,
+                color: 'white',
+                '&:hover': {
+                  backgroundColor: paginationColor,
+                  color: 'white',
+                },
+              },
+            },
+          },
+        },
+        MuiIconButton: {
+          styleOverrides: {
+            root: {
+              color: paginationColor,
+            },
+          },
+        },
+        MuiButtonBase: {
+          styleOverrides: {
+            root: {
+              color: paginationColor,
+            },
+          },
+        },
+        MuiTablePagination: {
+          styleOverrides: {
+            toolbar: {
+              '& p.MuiTablePagination-displayedRows': {
+                color: isDark ? 'white' : 'black',
+              },
+            },
+          },
+        },
+      },
+    });
 
+    return (
+      <ThemeProvider theme={theme}>
+        <GridPagination {...props} />
+      </ThemeProvider>
+    );
+  };
 
   return (
-    <div className='stats-grid'>
+    <div
+      className='stats-grid'
+      style={{
+        boxShadow: isDark
+          ? '1.25em 1.25em 3.75em rgb(40, 43, 113), -0.625em -0.625em 1.3125em #282b71'
+          : '0px 8px 6px rgba(0, 0, 0, 0.3), 0px -8px 6px rgba(0, 0, 0, 0.3)',
+      }}
+    >
       <Box style={{ bottom: 0, width: '100%', overflow: 'hidden' }}>
-        <div style={{ backgroundColor: 'rgb(51, 51, 51)', padding: '16px' }}>
+        <div style={{ padding: '16px' }}>
           {/* <ReturnToDialogButton onClick={handleGridClose} /> */}
-          <Button
-            variant='outlined'
-            color='primary'
+          <Fab
+        sx={{ left: 0, marginBottom: '10px' , boxShadow: '6px 6px 6px rgba(0, 0, 0, 0.2)' }}
+        color='secondary'
+        size='small'
+        aria-label='back'
             onClick={() => {
               handleBackClick();
               handleClickOpen();
             }}
           >
-            &lt; &lt; Back to Select
-          </Button>
+            {/* &lt; &lt; Back to Select */}
+            <ArrowBackIosNewIcon fontSize='small' />
+          </Fab>
           <DataGrid
+            components={{
+              Pagination: CustomPagination,
+            }}
             // rows={statsWithIds}
             rows={rows}
             columns={columns}
@@ -129,9 +199,10 @@ const statsWithIds = stats.reverse().map((row, index) => {
             // disableRowSelectionOnClick
             style={{
               opacity: 1,
-              background:
-                'linear-gradient(128deg, rgb(42, 164, 71) 0%, rgb(104, 194, 125) 100%) rgb(123, 231, 149)',
-              color: '#1d1c1c',
+              color: isDark ? 'white' : 'black',
+              background: isDark
+                ? 'linear-gradient(145deg, #1e2062, #030312)'
+                : 'linear-gradient(145deg, #3cc6f6, #d8f1ff)',
             }}
           />
         </div>
