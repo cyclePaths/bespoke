347b49f6b7e8dcc0ee821cba75b5ca1f8d5d2bd4
ernestquiambao <ernest.quiambao@gmail.com>
2023-05-31 10:48:11 -0500

    seedOpsparkReports

--- database/prisma/reportsSeed.js
+++ database/prisma/reportsSeed.js
@@ -7,8 +7,8 @@ let reportsSeed = [
     title: 'Large Sinkhole',
     body: 'On the right side only.  Road crews on it.',
     type: 'Road Hazard',
-    location_lat:  29.924974073488798, //  29.924974073488798, -90.08259640916626 6th and constance
-    location_lng: -90.08259640916626,
+    location_lat:  29.96432049046433,
+    location_lng: -90.05296397614092,
     userId: 1,
     imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685461347/1_xql8pq.png',
   },
@@ -16,7 +16,7 @@ let reportsSeed = [
 //     title:
 //     body:
 //     type: 'Road Hazard',
-//     location_lat:
+//     location_lat:  //  29.96432049046433, -90.05296397614092
 //     location_lng:
 //     userId:
 //     imgUrl:
@@ -53,8 +53,8 @@ let reportsSeed = [
     title: 'Lock Properly!',
     body: 'Always hate to see this.  Front wheel was a quick release.  Remember to lock your frames people!',
     type: 'Theft Alert',
-    location_lat:   29.925067058054037,//  29.925067058054037, -90.07818685768302  second and annunciation
-    location_lng:   -90.07818685768302,
+    location_lat:    29.964692281095083,
+    location_lng:   -90.05043197101236,
     userId: 1,
     imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685461940/Screen_Shot_2023-04-30_at_9.01.59_PM_o0e7ve.png',
   },
@@ -62,7 +62,7 @@ let reportsSeed = [
 //     title:
 //     body:
 //     type: 'Theft Alert',
-//     location_lat:
+//     location_lat:  //  29.964692281095083, -90.05043197101236
 //     location_lng:
 //     userId:
 //     imgUrl:
@@ -95,12 +95,21 @@ let reportsSeed = [
 //     imgUrl:
 //   },
   //    COLLISION
-    {
+  //   {
+  //   title: 'Biker hit!',
+  //   body: 'Biker was riding in the crosswalk and driver did not look right.  Both are to blame but please stop riding on the sidewalk people!',
+  //   type: 'Collision',
+  //   location_lat:   29.925067058054037, //  29.925067058054037, -90.08247839181261
+  //   location_lng:   -90.08247839181261,
+  //   userId: 1,
+  //   imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685461637/Screen_Shot_2023-04-30_at_9.08.50_PM_xt5vu8.png',
+  // },
+  {
     title: 'Biker hit!',
     body: 'Biker was riding in the crosswalk and driver did not look right.  Both are to blame but please stop riding on the sidewalk people!',
     type: 'Collision',
-    location_lat:   29.925067058054037, //  29.925067058054037, -90.08247839181261
-    location_lng:   -90.08247839181261,
+    location_lat:   29.963446777007203,
+    location_lng:   -90.0498096985655,
     userId: 1,
     imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685461637/Screen_Shot_2023-04-30_at_9.08.50_PM_xt5vu8.png',
   },
@@ -108,7 +117,7 @@ let reportsSeed = [
 //     title:
 //     body:
 //     type: 'Collision',
-//     location_lat:
+//     location_lat:  //  29.963446777007203, -90.0498096985655
 //     location_lng:
 //     userId:
 //     imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685461637/Screen_Shot_2023-04-30_at_9.08.50_PM_xt5vu8.png',
@@ -142,12 +151,21 @@ let reportsSeed = [
 //   },
 
   //    POI
+  // {
+  //   title:  'Bike Repair Station',
+  //   body: 'Pump works!  Used it to adjust my seat height.',
+  //   type: 'Point of Interest',
+  //   location_lat:   29.92348630863214,//  29.92348630863214, -90.07438884997833 soraparu and rousseau
+  //   location_lng:   -90.07438884997833,
+  //   userId: 1,
+  //   imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685462099/Screen_Shot_2023-04-30_at_9.00.47_PM_zh3nmr.png'
+  // },
   {
     title:  'Bike Repair Station',
     body: 'Pump works!  Used it to adjust my seat height.',
     type: 'Point of Interest',
-    location_lat:   29.92348630863214,//  29.92348630863214, -90.07438884997833 soraparu and rousseau
-    location_lng:   -90.07438884997833,
+    location_lat:   29.962071127486315,
+    location_lng:   -90.05390811364649,
     userId: 1,
     imgUrl: 'https://res.cloudinary.com/dcecaxmxv/image/upload/v1685462099/Screen_Shot_2023-04-30_at_9.00.47_PM_zh3nmr.png'
   },
@@ -155,7 +173,7 @@ let reportsSeed = [
 //     title:
 //     body:
 //     type: 'Point of Interest',
-//     location_lat:
+//     location_lat:  //  , -90.05390811364649
 //     location_lng:
 //     userId:
 //     imgUrl:
